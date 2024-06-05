9f9a931a636c12e726a21b8609c9c52e772b6c99
McCambley <jakemccamb16@gmail.com>
2024-06-05 00:47:30 -0400

    Add a few more lines

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
 print("Hello, World!")
 age = input("How old are you? ")
 print("It's pretty cool that your are " + age + " years old!")
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in number:
+    if i == 5:
+        print("I've got 5 on it!")
+    print("The square of " + str(i) + " is " + str(i * i))
+print("Goodbye, World!")
+
